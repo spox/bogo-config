f048bc67a00e2075f27e0ec750954f2f396f3b3e
Chris Roberts <code@chrisroberts.org>
2022-12-19 13:11:16 -0800

    Update tests with wrapped methods

--- test/specs/config_xml_spec.rb
+++ test/specs/config_xml_spec.rb
@@ -10,16 +10,15 @@ describe Bogo::Config do
     let(:config){ @config }
 
     it 'should provide valid config' do
-      config.get(:base, :fubar, :feebar).must_equal true
+      _(config.get(:base, :fubar, :feebar)).must_equal true
     end
 
     it 'should merge files in sorted order' do
-      config.get(:base, :bang).must_equal 'boom'
-      config.get(:base, :blam).must_equal false
-      config.get(:base, :fubar, :complete).must_equal 3.0
-      config[:last_file].must_equal true
+      _(config.get(:base, :bang)).must_equal 'boom'
+      _(config.get(:base, :blam)).must_equal false
+      _(config.get(:base, :fubar, :complete)).must_equal 3.0
+      _(config[:last_file]).must_equal true
     end
 
   end
-
 end
